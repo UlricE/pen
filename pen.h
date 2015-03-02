4fbf4912f80a1ea7c9c4dff8ddbc6bfd3d1cff5d
Ulric Eriksson <ulric@virtualbob.gullringe.se>
2015-03-02 18:16:14 +0100

    .

--- pen.h
+++ pen.h
@@ -54,11 +54,3 @@ extern void *pen_malloc(size_t);
 extern void *pen_realloc(void *, size_t);
 extern void mainloop(void);
 
-/* dlist.c */
-extern int dlist_insert(int, int);
-extern int dlist_remove(int);
-extern void dlist_free(int);
-extern int dlist_next(int);
-extern int dlist_value(int);
-extern void dlist_init(int);
-
