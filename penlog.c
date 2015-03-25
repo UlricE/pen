3f7f957e595fed6c1f0ceefb3eac00138d812087
Belden Lyman <belden.lyman@gmail.com>
2015-03-25 10:45:57 -0400

    Add LICENSE.md, update copyrights
    LICENSE.md: This is https://www.gnu.org/licenses/gpl-2.0.html,
      formatted for markdown
    
    Copyrights: added and updated ending dates to -2015 across the
      project

--- penlog.c
+++ penlog.c
@@ -1,5 +1,5 @@
 /*
-   Copyright (C) 2000-2013  Ulric Eriksson <ulric@siag.nu>
+   Copyright (C) 2000-2015  Ulric Eriksson <ulric@siag.nu>
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
