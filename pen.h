4a8d588d7f5135ec1f2d1a357826a5f4c162fade
ulric <ulric@debian>
2015-04-06 20:58:50 +0200

    .

--- pen.h
+++ pen.h
@@ -34,6 +34,38 @@ extern int service_main(int, char **);
 #define socket_errno errno
 #endif
 
+#ifdef HAVE_LIBSSL
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#endif	/* HAVE_LIBSSL */
+
+/* Connection states, as used in struct conn */
+#define CS_UNUSED	(0)	/* not connected, unused slot */
+#define CS_IN_PROGRESS	(1)	/* connection in progress */
+#define CS_CONNECTED	(2)	/* successfully connected */
+#define CS_CLOSED_UP	(4)	/* we read eof from upfd */
+#define CS_CLOSED_DOWN	(8)	/* we read eof from downfd */
+#define CS_CLOSED	(CS_CLOSED_UP | CS_CLOSED_DOWN)
+
+typedef struct {
+	int state;		/* as per above */
+	time_t t;		/* time of connection attempt */
+	int downfd, upfd;
+	unsigned char *downb, *downbptr, *upb, *upbptr;
+	int downn, upn;		/* pending bytes */
+	unsigned long ssx, srx;	/* server total sent, received */
+	unsigned long csx, crx;	/* client total sent, received */
+	int client;		/* client index */
+	int initial;		/* first server tried */
+	int server;		/* server index */
+	int pend;		/* node in pending_list */
+#ifdef HAVE_LIBSSL
+	SSL *ssl;
+	time_t reneg;		/* last time client requested renegotiation */
+#endif
+} connection;
+
+
 extern void (*event_add)(int, int);
 extern void (*event_arm)(int, int);
 extern void (*event_delete)(int);
@@ -42,6 +74,7 @@ extern int (*event_fd)(int *);
 
 extern int timeout;
 extern int connections_max;
+extern time_t now;
 
 extern void select_init(void);
 extern void poll_init(void);
